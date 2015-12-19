86dfc25c874e69674756a4d28772e46bf813cb7d
Richard Alexander Smith <smithra@usc.edu>
2015-12-19 15:10:48 -0800

    Initial Commit

---
+++ lynda exercises/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lynda exercises
+//
+//  Created by alex smith on 12/19/15.
+//  Copyright © 2015 alex smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lynda exercises/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  lynda exercises
+//
+//  Created by alex smith on 12/19/15.
+//  Copyright © 2015 alex smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
